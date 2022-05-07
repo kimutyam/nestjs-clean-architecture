4ae1ccfe24bab8219d0f678238e4751c0b6eb16c
kimutyam <devkimura@gmail.com>
2022-05-07 15:50:48 +0900

    命名をCatAgeに統一

--- src/domain/cat/catAge.ts
+++ src/domain/cat/catAge.ts
@@ -3,12 +3,12 @@ import { iso, prism } from 'newtype-ts';
 import type { CatGeneration } from './catGeneration';
 import { CAT_GENERATION } from './catGeneration';
 
-export type Age = Newtype<{ readonly CAT_ID: unique symbol }, number>;
-export const Age = {
-  iso: iso<Age>(),
-  prism: prism<Age>((n) => n >= 0),
-  judge: (age: Age): CatGeneration => {
-    const raw = Age.iso.unwrap(age);
+export type CatAge = Newtype<{ readonly CAT_AGE: unique symbol }, number>;
+export const CatAge = {
+  iso: iso<CatAge>(),
+  prism: prism<CatAge>((n) => n >= 0),
+  judge: (age: CatAge): CatGeneration => {
+    const raw = CatAge.iso.unwrap(age);
     if (raw >= 0 && raw <= 2) {
       return CAT_GENERATION.Junior;
     }
